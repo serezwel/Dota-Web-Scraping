868af4b495b698e69539f0233df0971abd6fa24b
Andrew Ngadiman <serezwel@gmail.com>
2021-11-15 16:18:38 +0700

    Finished web scraping data for multiple games series and casting to dataframe

--- webscrape.py
+++ webscrape.py
@@ -4,28 +4,23 @@ import pandas as pd
 import re
 import sys
 
-#NEED TO IMPLEMENT MORE THAN BO1
-#NEED TO FIX FINDING WINNER
-
-"""For BO3, go to div class vm-stats-container -> get all vm-stats-game -> same thing with BO1"""
-
 #function to get team agents and result
 def get_match_data(match_url):
     game_data = []
-    match_data = []
     page = requests.get(match_url)
     match_src = page.content
     match_soup = BeautifulSoup(match_src, 'lxml')
-    get_matches = match_soup.find_all("div", attrs = {"class": "vm-stats-game"})
+    get_matches = match_soup.find("div", attrs = {"class": "vm-stats-container"})
+    get_matches = get_matches.find_all("div", attrs = {"data-game-id" : re.compile('[0-9]+')})
     for matches in get_matches:
+        match_data = [] 
         get_agents_tag = matches.find_all("td", attrs={"class" : "mod-agents"})
         #Get 10 agents in the game
         for agents in get_agents_tag:
             match_data.append(agents.find("img").attrs["alt"])
-        print(match_data)
         get_map = matches.find("div", attrs={"class" : "map"})
         #Clean map text
-        game_map = re.search("Split|Bind|Ascent|Icebox|Haven|Breeze", get_map.text).group()
+        game_map = re.search("Split|Bind|Ascent|Icebox|Haven|Breeze|Fracture", get_map.text).group()
         #Append map into match data
         match_data.append(game_map)
         get_team2score = matches.find("div", attrs={"class" : "team mod-right"})
@@ -37,7 +32,6 @@ def get_match_data(match_url):
         else:
             match_data.append("Team 1")
         game_data.append(match_data)
-    
     return game_data
 
 
@@ -46,8 +40,9 @@ def get_match_data(match_url):
 match_details = ["Team1Agent1", "Team1Agent2", "Team1Agent3", "Team1Agent4", "Team1Agent5",
  "Team2Agent1", "Team2Agent2", "Team2Agent3", "Team2Agent4", "Team2Agent5", "Map", "Victory"]
 #create dataframe for matches
-match_df = pd.DataFrame(columns = match_details)
+match_df = pd.DataFrame()
 #loop through pages
+
 for i in range(1, 2):
     url = "https://www.vlr.gg/matches/results/?page=" + str(i)
     result_page = requests.get(url)
@@ -59,3 +54,6 @@ for i in range(1, 2):
         for match in matches_list:
             match_url = "https://www.vlr.gg" + match.attrs["href"]
             match_data = get_match_data(match_url)
+            match_df = match_df.append(match_data, ignore_index = True)
+match_df.columns = match_details
+print(match_df)
